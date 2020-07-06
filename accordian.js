1156ceeda5e7b47d42db4e0b0d5c4ff9aa2b7210
pavanichitrapu <63398525+pavanichitrapu@users.noreply.github.com>
2020-07-06 11:41:34 +0530

    Update accordian.js

--- accordian.js
+++ accordian.js
@@ -1,6 +1,7 @@
 var hamburger=document.getElementById('ham');
 var accord=document.getElementById('accordian');
-
+var sidebar=document.getElementById('sidebar');
+//sidebar.appendChild(accord);
 hamburger.addEventListener('click',function(){
     accord.style.display="block";
   });
@@ -8,3 +9,4 @@ var closeBtn=document.getElementById('cls-btn');
 closeBtn.addEventListener('click',function(){
    accord.style.display="none";
 });
+
